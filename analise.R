e25f3ad950d6c2b3df3630c2d766087ee366a44a
Rogério Guerra da Silveira <rogeriogsilveira@gmail.com>
2019-02-28 11:15:41 -0300

    Add files via upload

--- analise.R
+++ analise.R
@@ -1,51 +1,74 @@
 library("tidyverse")
 
-filter(tran2007, is.na(br))
+ggpltget_tran <- function(ano) {
+  library(readxl)
+  arquivo <- paste0("C:/Users/Samsung2/Google Drive/Acidentes/datatran", ano, ".xlsx")
+  read_xlsx(arquivo,
+            col_types = c("numeric", "date", "text", 
+                          "skip", "text", "text", "numeric", 
+                          "text", "text", "text", "text", "text", 
+                          "text", "text", "text", "text", "text", 
+                          "numeric", "numeric", "numeric", 
+                          "numeric", "numeric", "numeric", 
+                          "numeric", "numeric", "numeric"),
+            na = c("", "Ignorado", "Ignorada", "Nao Informado"))
+}
 
-mortos2007 <- tran2007 %>% 
-  group_by(uf) %>%
+tran <- rbind(tran2007, tran2008, tran2009, tran2010, tran2011, tran2012, tran2013, tran2014, tran2015, tran2016, tran2017)
+
+tran <- as.tibble(tran)
+
+tran_data <- tran[complete.cases(tran),]
+
+mortos_2007_2017 <- tran %>% 
+  group_by(ano) %>%
   summarize(mortes = sum(mortos > 0)) %>% 
-  arrange(desc(mortes))
-  
-mortos2008 <- tran2008 %>% 
-  group_by(uf) %>%
-  summarize(mortes = sum(mortos > 0)) %>% 
-  arrange(desc(mortes))
+  arrange(ano)
 
-mortos2009 <- tran2009 %>% 
-  group_by(uf) %>%
-  summarize(mortes = sum(mortos > 0)) %>% 
-  arrange(desc(mortes))
+mortes <- mortes %>% 
+  full_join(mortos2017, by = "uf")
 
-mortos <- mortos2007 %>% 
-  inner_join(mortos2008, by = "uf")
+colnames(mortes) <- c('uf', '2007', '2008', '2009', '2010', '2011', '2012','2013', '2014', '2015', '2016', '2017')
 
-colnames(mortos)[c(2,3)] <- c('2007', '2008')
-
-mortos <- mortos %>% 
-  inner_join(mortos2009, by = "uf")
-
-colnames(mortos)[4] <- '2009'
-
-ggplot(data = mortos2007) +
+ggplot(data = mortes) +
   geom_col(mapping = aes(x = uf, y = mortes, fill = uf))
 
-mortos2010 <- tran2010 %>% 
-  group_by(uf) %>%
+causa_acidente_levels <- unique(tran$causa_acidente)
+
+tran$causa_acidente <- factor(tran$causa_acidente, levels = causa_acidente_levels)
+
+library("OneR")
+modelo <- OneR(classificacao_acidente ~.,data = tran, verbose = TRUE)
+summary(modelo)
+
+tran_com_vitimas <- tran$mortos > 0 | tran$feridos_leves > 0 | tran$feridos_graves > 0
+tran_com_mortes <- tran$mortos > 0
+tran_vitimas <- tran[tran_com_vitimas, ]
+tran_mortes <- tran[tran_com_mortes, ]
+
+mortes_por_causa <- tran %>% 
+  group_by(causa_acidente) %>% na.omit() %>% 
   summarize(mortes = sum(mortos > 0)) %>% 
   arrange(desc(mortes))
 
-mortos <- mortos %>%
-  inner_join(mortos2010, by = "uf")
-
-colnames(mortos)[5] <- '2010'
-
-mortos2011 <- tran2011 %>% 
-  group_by(uf) %>%
+mortes_por_tipo <- tran %>% 
+  group_by(tipo_acidente) %>% na.omit() %>% 
   summarize(mortes = sum(mortos > 0)) %>% 
   arrange(desc(mortes))
 
-mortos <- mortos %>% 
-  inner_join(mortos2011, by = "uf")
+x <- tran %>% group_by(causa_acidente, tipo_acidente) %>% 
+     count(mortos > 0 | feridos_graves > 0)
+x <- x[x$`mortos > 0 | feridos_graves > 0`,]
+causa_tipo <- x %>% filter(causa_acidente, tipo_acidente, n)
 
-colnames(mortos)[6] <- '2011'
+vitimas <- tran %>% 
+           group_by(ano) %>% 
+           summarize(vitimas = sum(mortos>0) + sum(feridos)) %>% 
+           arrange(ano)
+
+ggplot(data = vitimas, mapping = aes(x = ano, y = vitimas), ylab= "mortos e feridos graves", xlab = "ano") +
+  geom_point() +
+  geom_smooth(method = lm, formula = y ~ poly(x, 4))
+
+
+
